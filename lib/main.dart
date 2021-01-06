2076454cebf4a98e6036431315f213b79f576a92
Joshua Wacks <2143116@students.wits.ac.za>
2021-01-06 20:03:30 +0200

    Colour background

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
       fontSize: 22,
       fontFamily: 'Raleway',
       fontStyle: FontStyle.normal,
-      color: Colors.blueGrey,
+      color: Colors.black,
+      decorationColor: Colors.black,
     );
 
     TextField usernameTxtField = new TextField(
@@ -42,30 +44,65 @@ class MyApp extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildDataColumn(
-              Icons.person,  usernameTxtField),
-          _buildDataColumn(
-              Icons.enhanced_encryption, passwordTxtField)
+          _buildDataColumn(Icons.person, usernameTxtField),
+          _buildDataColumn(Icons.enhanced_encryption, passwordTxtField)
         ],
       ),
     );
 
+    TextStyle buttonTextStyle = new TextStyle(
+      fontSize: 18,
+      fontFamily: 'Raleway',
+      fontStyle: FontStyle.normal,
+    );
+
+    Container loginButton = new Container(
+      padding: EdgeInsets.fromLTRB(50, 40, 50, 0),
+      child: FloatingActionButton.extended(
+        backgroundColor: Colors.black,
+        onPressed: () {} ,
+        icon: Icon(Icons.login),
+        label: Text("Login",style: buttonTextStyle,),
+      ),
+    );
+
+    Container userButtons= new Container(
+      padding: EdgeInsets.fromLTRB(32, 40, 32, 0),
+
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton.extended(
+              label : Text('   Forgot\nPassword',style: buttonTextStyle,),
+              backgroundColor: Colors.black,
+              onPressed: () {}
+          ),
+          FloatingActionButton.extended(
+              label : Text('Sign\n Up',style: buttonTextStyle,),
+              backgroundColor: Colors.black,
+              onPressed: () {}
+          ),
+
+        ],
+      )
+    );
+
+
+
     return MaterialApp(
-        title: 'Petrol Perfect',
-        home: Container(
+      title: 'Petrol Perfect',
+      home: Container(
           decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("images/black_green.jpg"),fit: BoxFit.cover)),
-            child: Scaffold(
-              backgroundColor: Colors.transparent,
-              body: ListView(
-                children: [
-                  titleSection,
-                  dataSection,
-                ],
-              ),
-            )),
-            );
+              image: DecorationImage(
+                  image: AssetImage("images/gradient_background.jpg"),
+                  fit: BoxFit.cover)),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: ListView(
+              children: [titleSection, dataSection, loginButton,userButtons],
+            ),
+          )),
+    );
   }
 
   Widget titleSection = Container(
@@ -79,7 +116,7 @@ class MyApp extends StatelessWidget {
             Text(
               'Petrol Perfect',
               style: TextStyle(
-                  color: Colors.yellowAccent,
+                  color: Color.fromARGB(255, 12, 90, 24),
                   fontSize: 47.0,
                   wordSpacing: 2.0,
                   letterSpacing: 3.0,
@@ -104,13 +141,14 @@ class MyApp extends StatelessWidget {
         children: [
           Icon(
             icon,
-            color: Colors.blueGrey,
+            color: Colors.lightBlue,
             size: 40,
           ),
-          Expanded(
-              child: textField)
+          Expanded(child: textField)
         ],
       ),
     );
   }
+
+
 }
